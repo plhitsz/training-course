2ad871438acf029d2ffb575daa1ce2108edce480
plhitsz <plhitsz@outlook.com>
2023-04-27 21:04:43 +0800

    feat: how to save pointer of CRTP class

--- crtp_exmple.cc
+++ crtp_exmple.cc
@@ -3,17 +3,22 @@
 template <class Derived>
 class Base {
  public:
-  void name() { (static_cast<Derived*>(this))->impl(); }
+  std::string& name() { (static_cast<Derived*>(this))->impl(); }
 };
 
 class D1 : public Base<D1> {
  public:
-  void impl() { std::puts("D1::impl()"); }
+  std::string& impl() { return msg_; }
+ private:
+  std::string msg_ = {"D2::impl()"};
 };
 class D2 : public Base<D2> {
  public:
-  void impl() { std::puts("D2::impl()"); }
+  std::string& impl() { return msg_; }
+ private:
+  std::string msg_ = {"D2::impl()"};
 };
+
 // reference version
 template <class Derived>
 void handle(Base<Derived>& dd) {
@@ -25,6 +30,21 @@ template <typename Derived>
 void handle2(std::shared_ptr<Base<Derived>>& dd) {
   dd->name();
 }
+
+// set pointer type of CRTP class
+class Test {
+ public:
+  // save ptr to handler_
+  template <typename Derived>
+  void setHandler(std::shared_ptr<Base<Derived>> dd) {
+    handler_ = dd;
+  }
+  // get name by reference
+  std::string& getName() {
+    std::visit([](auto& handler)->std::string& { handler->name(); }, handler_);
+  }
+  std::variant<std::shared_ptr<Base<D1>>, std::shared_ptr<Base<D2>>> handler_;
+};
 
 // using example
 void test() {
@@ -41,6 +61,11 @@ void test() {
   std::shared_ptr<Base<D2>> d22 = std::make_shared<D2>();
   handle2(d11);
   handle2(d22);
+ 
+  Test tt;
+  std::shared_ptr<Base<D1>> d1_ptr = std::make_shared<D1>();
+  tt.setHandler(d1_ptr);
+  tt.getName();
 }
 
 int main() { test(); }
