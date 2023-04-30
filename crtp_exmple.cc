511cf1bcd5759addf33b9734c649262d297da1ff
plhitsz <plhitsz@outlook.com>
2023-04-30 11:22:22 +0800

    chore: update crtp

--- crtp_exmple.cc
+++ crtp_exmple.cc
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <memory>
+#include <variant>
 template <class Derived>
 class Base {
  public:
-  std::string& name() { (static_cast<Derived*>(this))->impl(); }
+  std::string& name() { return (static_cast<Derived*>(this))->impl(); }
 };
 
 class D1 : public Base<D1> {
  public:
   std::string& impl() { return msg_; }
+
  private:
   std::string msg_ = {"D2::impl()"};
 };
 class D2 : public Base<D2> {
  public:
   std::string& impl() { return msg_; }
+
  private:
   std::string msg_ = {"D2::impl()"};
 };
@@ -41,9 +44,29 @@ class Test {
   }
   // get name by reference
   std::string& getName() {
-    std::visit([](auto& handler)->std::string& { handler->name(); }, handler_);
+    return std::visit(
+        [](auto& handler) -> std::string& { return handler->name(); },
+        handler_);
+  }
+
+  template <typename Derived>
+  void setHandler2(std::shared_ptr<Base<Derived>> dd) {
+    if constexpr (std::is_same_v<Derived, D1>) {
+      h1_ = dd;
+    } else if constexpr (std::is_same_v<Derived, D2>) {
+      h2_ = dd;
+    }
+  }
+  std::string& getName2() {
+    if (h1_ != nullptr) {
+      return h1_->name();
+    } else if (h2_ != nullptr) {
+      return h2_->name();
+    }
   }
   std::variant<std::shared_ptr<Base<D1>>, std::shared_ptr<Base<D2>>> handler_;
+  std::shared_ptr<Base<D1>> h1_;
+  std::shared_ptr<Base<D2>> h2_;
 };
 
 // using example
@@ -61,11 +84,11 @@ void test() {
   std::shared_ptr<Base<D2>> d22 = std::make_shared<D2>();
   handle2(d11);
   handle2(d22);
- 
+
   Test tt;
   std::shared_ptr<Base<D1>> d1_ptr = std::make_shared<D1>();
   tt.setHandler(d1_ptr);
-  tt.getName();
+  std::cout << tt.getName() << std::endl;
 }
 
 int main() { test(); }
